7da2b6c0d1fca952a5e051c512955677d5505884
zhengzhuang95 <zheng951203@163.com>
2019-03-14 17:03:17 +0800

    second

---
+++ lib/config/service_url.dart
@@ -0,0 +1,4 @@
+const serviceUrl = 'http://test.baixingliangfan.cn/baixing/';
+const servicePath = {
+  'homePageContext': serviceUrl + 'wxmini/homePageContent', // 商家首页信息
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,59 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
+import '../service/service_method.dart';
 
 class HomePage extends StatefulWidget {
+  @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController = TextEditingController();
-  String showText = '欢迎您来到美好人间高级会所';
+  String homePageContent = '正在获取数据';
+
+  @override
+  void initState() {
+    getHomePageContent().then((val){
+      setState(() {
+       homePageContent=val.toString(); 
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('美好人间'),
-        ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: typeController,
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(10.0),
-                    labelText: '美女类型',
-                    helperText: '请输入你喜欢的类型'),
-                autofocus: false,
-              ),
-              RaisedButton(
-                onPressed: () {},
-                child: Text('选着完毕'),
-              ),
-              Text(
-                showText,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              ),
-            ],
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('百姓生活+'),
+      ),
+      body: SingleChildScrollView(
+        child: Text(homePageContent),
       ),
     );
-  }
-
-  Future getHttp(String TypeText) async {
-    try {
-      Response response;
-      var data = {'name': TypeText};
-      response = await Dio().get(
-        "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian",
-        queryParameters: data
-      );
-      return response.data;
-    } catch (e) {
-      return print(e);
-    }
   }
 }
